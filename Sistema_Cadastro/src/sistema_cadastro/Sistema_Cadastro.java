348d30fddd2785293e3bfb1e3c1e325cd9b8cda7
IrisSay <iris.skf@gmail.com>
2025-02-01 14:40:39 -0300

    Add files via upload

---
+++ Sistema_Cadastro/src/sistema_cadastro/Sistema_Cadastro.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_cadastro;
+
+import com.cadastro.telas.Login;
+
+/**
+ *
+ * @author Professor
+ */
+public class Sistema_Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login telaL = new Login();
+        telaL.setVisible(true);
+        telaL.setLocationRelativeTo(null);
+    }
+    
+}
